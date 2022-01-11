8512e3718c523d24482eb5abd7cb787afe0e080f
Brooke <brookepaglia@gmail.com>
2022-01-11 18:14:21 -0500

    open high score modal and display scores

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 // -----------------------
 const main = document.querySelector('.content');
 const time = document.querySelector('.time');
+const modal = document.querySelector('.modal');
 const questions = [
   { question: 'True or False?', options: ['true', 'false'], answer: 'true' },
   { question: 'One or two?', options: ['one', 'two'], answer: 'one' },
@@ -38,9 +39,12 @@ let startQuiz = function () {
 };
 
 let showQuestion = function () {
+  // If there's still time on the clock and questions available to be answered
   if (parseInt(currentTime) > 0 && availableQuestions.length > 0) {
     // Get a random question from the available questions
     let num = Math.floor(Math.random() * availableQuestions.length);
+    // Get the question from the questions object that matches the question number
+    // from the available pool
     let currQ = questions[availableQuestions[num]];
 
     // Display the question in the main element
@@ -55,46 +59,67 @@ let showQuestion = function () {
       btn.addEventListener('click', answerHandler);
       main.appendChild(btn);
     }
-    // Remove the question from the array so we don't get it again
-    console.log(num, availableQuestions, availableQuestions.indexOf(num));
+    // Remove the question from the available pool so we don't get it again
     availableQuestions.splice(availableQuestions.indexOf(num), 1);
-    console.log(availableQuestions);
   } else {
+    // We're either out of time or questions, end the game
     return endGame();
   }
 };
 
 let answerHandler = function (event) {
+  // Get the question key
   let key = event.target.dataset['key'];
+  // Get the answer text from the button
   let answer = event.target.textContent
     .substring(event.target.textContent.indexOf('. ') + 1)
     .trim();
+  // Create the Correct / Wrong answer div
   let response = document.createElement('div');
   response.className = 'response';
-  console.log(response);
+
   if (answer == questions[key].answer) {
+    // If the button matches the answer on the question object
+    // it's correct
     response.textContent = 'Correct!';
   } else {
+    // The answer was wrong, deduct 10s from the timer
     response.textContent = 'Wrong!';
     currentTime = Math.max(currentTime - 10, 0);
     time.textContent = currentTime;
   }
-  console.log(main);
+
+  // Set the response answer div to go away after being
+  // shown for 2 seconds
   setTimeout(() => {
     response.remove();
   }, 2000);
+
+  // Show the next question
   showQuestion();
+
+  // Append the response to the content page, it will
+  // display under the current question's answer buttons
   main.appendChild(response);
 };
 
 let endGame = function () {
   if (currentTime === 0) {
+    // The player lost, the timer has run out
     alert("Time's up!");
   } else {
+    // The player won, they answered all questions with
+    // time left on the timer
     alert('You won!');
+
+    // Record the current timer value as the high score
     hiScore = currentTime;
+
+    // Set the timer to 0 so it stops counting down
     currentTime = 0;
   }
+
+  // Display the user's high score and form to save the score
   main.innerHTML = `
   <h1>All done!</h2>
   <p>Your final score is ${hiScore}.</p>
@@ -109,23 +134,53 @@ let endGame = function () {
 
 let saveHiScore = function (event) {
   event.preventDefault();
+  // Save current score to the highScores array
   highScores.push({ initials: event.target[0].value, score: hiScore });
-  console.log(highScores);
+  // Save the highScores array to localStorage
   localStorage.setItem('highScores', JSON.stringify(highScores));
+  // Reset the form and disable the submit button so the same score
+  // can't be submitted multiple times
   event.target.reset();
   document.querySelector('#submit').disabled = true;
 };
 
 let loadHighScores = function () {
+  // If the high scores value is empty, do nothing
+  // or load in the high scores from localStorage
   if (!highScores) {
     return false;
   }
   highScores = JSON.parse(localStorage.getItem('highScores'));
 };
 
+let showModal = function () {
+  modal.style.display = 'block';
+  let row;
+  for (let i = 0; i < highScores.length; i++) {
+    // Create a row
+    row = document.createElement('tr');
+
+    // Create columns with Rank (array index),
+    // initials, and the score
+    row.innerHTML = `
+      <td>${i + 1}</td>
+      <td>${highScores[i].initials}</td>
+      <td>${highScores[i].score}</td>
+    `;
+    // Add the row to the table
+    document.querySelector('table').appendChild(row);
+  }
+};
+
+const handleModalClick = function (event) {
+  console.log(event);
+};
+
 // -----------------------
 // EVENT HANDLERS
 // -----------------------
 document.querySelector('#start-quiz').addEventListener('click', startQuiz);
+document.querySelector('.view-hi-score').addEventListener('click', showModal);
+modal.addEventListener('click', handleModalClick);
 
 loadHighScores();
